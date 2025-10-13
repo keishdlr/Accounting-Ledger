7732078bb8b384f0ac53fc26cc6a0a702acea422
keisha De La Rosa <keishdlr@gmail.com>
2025-10-12 23:00:12 -0400

    added method to create a new file of the selected filtered report

--- src/main/java/com/pluralsight/Capstone/ReportService.java
+++ src/main/java/com/pluralsight/Capstone/ReportService.java
@@ -1,5 +1,8 @@
 package com.pluralsight.Capstone;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -34,21 +37,33 @@ public class ReportService {
             if (t.isPayment()) {
                 System.out.println(t.toCSV());
             }
-        }}
-        // Display transactions from the current month
-        public static void reportMonthToDate (List < Transaction > transactions) {
-           //Get today's date
-            LocalDate now = LocalDate.now();
-            // for loop to go through each transaction in the list
-            for (Transaction t : transactions) {
-                //nested if statement to check if the month and year is the same as today
-                if (t.getDate().getMonth() == now.getMonth() &&
-                        t.getDate().getYear() == now.getYear()) {
-                    // print the transaction that matches the year and month
-                    System.out.println(t.toCSV());
-                }
+        }
+    }
+
+    // Display transactions from the current month
+    public static void reportMonthToDate(List<Transaction> transactions) {
+        //Get today's date
+        LocalDate now = LocalDate.now();
+        // for loop to go through each transaction in the list
+        for (Transaction t : transactions) {
+            //nested if statement to check if the month and year is the same as today
+            if (t.getDate().getMonth() == now.getMonth() &&
+                    t.getDate().getYear() == now.getYear()) {
+                // print the transaction that matches the year and month
+                System.out.println(t.toCSV());
             }
         }
     }
+    // Export selected filtered list of transactions to a new CSV file
+     try (BufferedWriter writer = new BufferedWriter(new FileWriter("CustomReport.csv", true)) {
+        //writes the filtered transaction in the csv format (CustomReport.java) and eats the buffer
+        writer.write(t.toCSV()); // Assumes Transaction has a toCsv() method
+        writer.newLine();
+    } catch(IOException e)
+    {
+        //error message when writing fails
+        System.err.println("Error saving transaction: " + e.getMessage());
+    }}
+
 
 
